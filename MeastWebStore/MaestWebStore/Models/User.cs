7f2041b6ea115d02b271003e7758dd35d29f738e
BuildTools <unconfigured@null.spigotmc.org>
2016-06-07 18:37:28 +0200

    initial push

---
+++ MeastWebStore/MaestWebStore/Models/User.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace MaestWebStore.Models
+{
+    public class User
+    {
+        /// <summary>
+        /// A required username the user identifies themselves with.
+        /// </summary>
+        [Required]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        /// <summary>
+        /// The password (not hashed) the user used to log in.
+        /// </summary>
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// A boolean which if checked leaves the user logged in.
+        /// </summary>
+        [Display(Name = "Remember me on this computer")]
+        public bool RememberMe { get; set; }
+
+        
+        public bool IsValid(string _username, string _password)
+        {
+            string _sqlSelect = @"SELECT accountname FROM accounttable WHERE accountname = ':accountname' AND passwordhash = ':passwordhash'";
+            var cmd = new SqlCommand(_sqlSelect, Util.DatabaseConnection.Conn);
+            cmd.Parameters
+                .Add(new SqlParameter("accountname", SqlDbType.NVarChar))
+                .Value = _username;
+            cmd.Parameters
+                .Add(new SqlParameter("password", SqlDbType.NVarChar))
+                .Value = _password;
+            Util.DatabaseConnection.OpenConnection();
+
+            var dbReader = cmd.ExecuteReader();
+            if (dbReader.HasRows)
+            {
+                dbReader.Dispose();
+                cmd.Dispose();
+                return true;
+            }
+            else
+            {
+                dbReader.Dispose();
+                cmd.Dispose();
+                return false;
+            }
+        }
+    }
+}
---
+++ MeastWebStore/MaestWebStore/Util/DatabaseConnection.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace MaestWebStore.Util
+{
+    public static class DatabaseConnection
+    {
+        /// <summary>
+        /// The username used to connect with the database.
+        /// </summary>
+        private const string USER = "i319888";
+
+        /// <summary>
+        /// The password used to connect with the database.
+        /// </summary>
+        private const string PASSWORD = "Detunnel117!";
+
+        /// <summary>
+        /// The oracle-specific string used to connect with the database.
+        /// </summary>
+        //private const string CONNECTION_STRING = "User Id= " + USER + ";Password= " + PASSWORD + ";Data Source=" + @"//fhictora01.fhict.local" + ";";
+        private const string CONNECTION_STRING = "Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=fhictora01.fhict.local)(PORT=1521))(CONNECT_DATA =(SERVICE_NAME=fhictora)))" + "uid=" + USER + ";pwd=" + PASSWORD + ";";
+
+
+
+        public static readonly SqlConnection Conn;
+
+        static DatabaseConnection()
+        {
+            try
+            {
+                Conn = new SqlConnection(CONNECTION_STRING);
+            }
+            catch (SqlException oEx)
+            {
+                Console.WriteLine(@"Er kon geen verbinding met de Database worden gemaakt." + Environment.NewLine +
+                                @"Controleer of er een internet en VPN verbinding is." + Environment.NewLine +
+                                @"Error Message: " + oEx.Message + Environment.NewLine +
+                                @"Error Data: " + oEx.Data);
+            }
+        }
+
+        /// <summary>
+        /// Opens a connection with the database.
+        /// </summary>
+        /// <returns>A bool wether the connection was sucessfully opened</returns>
+        public static bool OpenConnection()
+        {
+            Console.WriteLine(Conn.State == System.Data.ConnectionState.Open ? "Database Connection was already open" : "Opening Database Connection..");
+            try
+            {
+                Conn.Open();
+                return true;
+            }
+            catch (SqlException oEx)
+            {
+                Console.WriteLine(@"Kon de databaseconnectie niet openen." + Environment.NewLine +
+                @"Controleer of er een internet en VPN verbinding is." + Environment.NewLine +
+                @"Error Message: " + oEx.Message + Environment.NewLine +
+                @"Error Data: " + oEx.Data);
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Closes the connection with the database.
+        /// </summary>
+        /// <returns>A bool wether the connection was successfully closed</returns>
+        public static bool CloseConnection()
+        {
+            Console.WriteLine(Conn.State == System.Data.ConnectionState.Closed ? "Database Connection was already closed" : "Closing Database Connection..");
+            try
+            {
+                Conn.Close();
+                return true;
+            }
+            catch (SqlException oEx)
+            {
+                Console.WriteLine(@"Kon de databaseconnectie niet sluiten." + Environment.NewLine +
+                @"Controleer of er een internet en VPN verbinding is." + Environment.NewLine +
+                @"Error Message: " + oEx.Message + Environment.NewLine +
+                @"Error Data: " + oEx.Data);
+                return false;
+            }
+        }
+    }
+}
